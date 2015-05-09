54d839fade951a9e504227f0b3e6e7520886aba6
Janusz Lenar <malleor@gmail.com>
2015-05-09 15:13:23 +0200

    Scraping all threads

--- scrape.py
+++ scrape.py
@@ -16,7 +16,7 @@ def get_stats(posts, stats={}):
                 stats[w] += 1
     return stats
 
-def get_posts(url):
+def get_posts_in_thread(url):
     # form the request
     url = urllib.quote(url)
     request = r'https://api.import.io/store/data/868ddafc-971b-4581-8810-5bf5f428553e/_query?input/webpage/url=%s&_user=f9a98378-b97f-4ef8-a5e4-d9ec01ac110c&_apikey=%s' % (url, KEY)
@@ -32,6 +32,26 @@ def get_posts(url):
     posts = [r['alt1_content'] for r in j['results'] if r['thead_value'] == op]
     
     return posts
+
+def get_all_posts():
+    # form the request
+    request = r'https://api.import.io/store/data/b721eb59-3521-4306-bfdb-0186331065b8/_query?input%2Fwebpage%2Furl=http%3A%2F%2Fwww.browar.biz%2Fforum%2Fforumdisplay.php%3Ff%3D301&_user=1e0a40f7-1454-4a5b-9214-e506d77db942&_apikey=' + KEY
+    
+    # load the data
+    print 'Fetching threads form the forum...'
+    j = json.loads(urllib.urlopen(request).read())
+    num_threads = len(j['results'])
+    print '...done (%d threads)' % num_threads
+    
+    # fetch all links and posts
+    posts_per_store = {}
+    for idx, r in enumerate(j['results']):
+        name = r['shop/_text']
+        url = r['shop']
+        
+        print 'Fetching posts for the shop %d of %d: \'%s\'...' % (1+idx, num_threads, name)
+        posts_per_store[name] = get_posts_in_thread(url)
+        print '...done'
 
 
 
@@ -57,7 +77,7 @@ def get_beer_stores(beer):
 
 
 def test():
-    posts = get_posts(r'http://www.browar.biz/forum/showthread.php?t=111581')
+    posts = get_posts_in_thread(r'http://www.browar.biz/forum/showthread.php?t=111581')
     stats = get_stats(posts)
     print sorted(stats.items(), key=lambda x: x[1])
 
