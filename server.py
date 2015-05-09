cc8e6f2ffed51725c9994a06556449db3d7ddf5d
Janusz Lenar <malleor@gmail.com>
2015-05-09 17:33:19 +0200

    Changed API

--- server.py
+++ server.py
@@ -11,9 +11,14 @@ _posts_per_store = scrape.get_all_posts()
 # config the server
 @_route('/find/<beer>')
 def find_stores(beer):
+    # find the stores
     stores = query.find_beer(_posts_per_store, beer)
     
-    return json.dumps(stores)
+    # form API
+    j = { 'beer_locations': [{ 'address_with_name': s } for s in stores] }
+    
+    # respond
+    return json.dumps(j)
 
 # run the server
 _run(host='0.0.0.0', port=8080)
